8c74e9e332a882672e48697c57f5b752efe8ba04
MrAmericanMike <american2010@hotmail.com>
2021-01-21 09:26:56 -0300

    Svelte-Base

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+	let world = "Mundo";
+</script>
+
+<main>
+	<h1>¡Hola {world}!</h1>
+</main>
+
+<style>
+	main {
+		text-align: center;
+	}
+	h1 {
+		color: royalblue;
+		text-transform: uppercase;
+		font-size: 6em;
+	}
+</style>
