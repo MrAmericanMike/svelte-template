261d5f1ea3fc58260e7199fef02c8417a9427073
MrAmericanMike <mramericanmike@gmail.com>
2022-06-30 12:55:00 -0300

    Libraries updates and reformating project structure

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,19 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
+	root: "src",
+	build: {
+		minify: true,
+		assetsInlineLimit: 0,
+		outDir: "../public",
+		rollupOptions: {
+			output: {
+				entryFileNames: "assets/[name].js",
+				chunkFileNames: "assets/[name].js",
+				assetFileNames: "assets/[name].[ext]"
+			}
+		}
+	},
 	plugins: [svelte()]
 });
 
