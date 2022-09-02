3287b1233f7e93141425fb725d77206c638c2882
MrAmericanMike <mramericanmike@gmail.com>
2022-09-02 14:06:04 -0300

    Cleaner start

--- src/main.js
+++ src/main.js
@@ -8,9 +8,13 @@ const createWindow = () => {
 	const mainWindow = new BrowserWindow({
 		width: 1280,
 		height: 720,
+		show: false,
 		webPreferences: {
 			nodeIntegration: true
 		}
+	});
+	mainWindow.once("ready-to-show", () => {
+		mainWindow.show();
 	});
 	mainWindow.loadURL("http://localhost:3000");
 	// mainWindow.loadFile("../public/index.html");
@@ -22,12 +26,11 @@ app.whenReady().then(() => {
 		if (BrowserWindow.getAllWindows().length === 0) {
 			createWindow();
 		}
-	})
+	});
 });
 
 app.on("window-all-closed", () => {
-	if (process.platform !== 'darwin') {
+	if (process.platform !== "darwin") {
 		app.quit();
 	}
 });
-
